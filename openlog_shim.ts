a98019dd35a961cf7037be1309f2faa624793da3
Raik Andritschke <58722460+CalliTGS3@users.noreply.github.com>
2024-09-10 16:50:36 +0200

    Update openlog_shim.ts

--- openlog_shim.ts
+++ openlog_shim.ts
@@ -1,4 +1,6 @@
 declare namespace Qwiic_Openlog{ 
     //% repeat.defl=0 shim=Qwiic_Openlog::i2cWriteBuffer
-    export function i2cWriteBuffer(address: int32, buf: Buffer, repeat?: boolean): int32;
+    export function i2cWriteBuffer(address: int32, buf: Buffer, repeat?: boolean): int32 {
+        return;
+    };
 }  
