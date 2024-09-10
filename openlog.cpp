19b0ce73d6cfdf380d949afb2d5591e668081c55
Raik Andritschke <58722460+CalliTGS3@users.noreply.github.com>
2024-09-10 15:52:33 +0200

    Update openlog.cpp

--- openlog.cpp
+++ openlog.cpp
@@ -1,5 +1,11 @@
 #include "pxt.h"
 
+#if MICROBIT_CODAL
+#define BUFFER_TYPE uint8_t*
+#else
+#define BUFFER_TYPE char*
+#endif
+
 namespace Qwiic_Openlog {
     //%
     int i2cWriteBuffer(int address, Buffer buf, bool repeat = false)
